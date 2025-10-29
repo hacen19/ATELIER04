aa1f62a74400da337d0c2062801ddacbeb761465
hacen19 <hassenhemich@gmail.com>
2025-10-29 11:30:31 -0400

    Ex7 - Commit 5: Trois instances creees et affichage nombreTotal

--- exercice7.dart
+++ exercice7.dart
@@ -2,8 +2,21 @@
 class Tache {
   String description;          // Propriété publique
   static int nombreTotal = 0;  // Propriété statique
+  
+  // Constructeur qui incrémente nombreTotal
+  Tache(this.description) {
+    nombreTotal++;
+  }
 }
 
 void main() {
-  print("Exercice 7 : Le Compteur de Tâches");
+  print("Exercice 7 : Le Compteur de Tâches\n");
+  
+  // Créer trois instances de Tache
+  var tache1 = Tache("Faire les courses");
+  var tache2 = Tache("Étudier Dart");
+  var tache3 = Tache("Faire l'exercice 7");
+  
+  // Afficher le nombre total de tâches
+  print("Nombre total de tâches créées : ${Tache.nombreTotal}");
 }
