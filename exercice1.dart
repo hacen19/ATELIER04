e20f282a1de0e7edd03ebd9e4930023c96c513e2
hacen19 <hassenhemich@gmail.com>
2025-10-29 03:31:15 -0400

    Ex1 - Commit 2: Constructeur Couleur(this.r, this.g, this.b)

---
+++ exercice1.dart
@@ -0,0 +1,13 @@
+// Classe Couleur
+class Couleur {
+  final int r;  // Rouge
+  final int g;  // Vert
+  final int b;  // Bleu
+  
+  // Constructeur principal
+  Couleur(this.r, this.g, this.b);
+}
+
+void main() {
+  print("Exercice 1 : L'usine de couleurs");
+}
