a83bdbe380c4b3e9f44b1f4772021a50b82623d9
hacen19 <hassenhemich@gmail.com>
2025-10-29 11:07:26 -0400

    Ex6 - Commit 3: Classe BaseDeDonnees implemente Connectable

---
+++ exercice6.dart
@@ -0,0 +1,35 @@
+// Interface Connectable
+class Connectable {
+  void connecter(String utilisateur) {}
+  void deconnecter() {}
+}
+
+// Classe ServeurAPI qui implémente Connectable
+class ServeurAPI implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("ServeurAPI : Connexion établie pour $utilisateur.");
+  }
+  
+  @override
+  void deconnecter() {
+    print("ServeurAPI : Déconnexion réussie.");
+  }
+}
+
+// Classe BaseDeDonnees qui implémente Connectable
+class BaseDeDonnees implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("BaseDeDonnees : Connexion établie pour $utilisateur.");
+  }
+  
+  @override
+  void deconnecter() {
+    print("BaseDeDonnees : Déconnexion réussie.");
+  }
+}
+
+void main() {
+  print("Exercice 6 : Le Contrat de Service");
+}
