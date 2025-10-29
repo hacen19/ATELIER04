24b0324d64a4952738dc301f7b5687a7274a3ddf
hacen19 <hassenhemich@gmail.com>
2025-10-29 04:13:05 -0400

    Ex3 - Commit 5: Getter solde formaté en dollar

--- exercice03.dart
+++ exercice03.dart
@@ -5,6 +5,11 @@ class Compte {
   final String numeroCompte;
   
   Compte(this.numeroCompte, [this._solde = 0.0]);
+  
+  // AJOUTEZ CETTE PARTIE ⬇️
+  String get solde {
+    return "${_solde.toStringAsFixed(2)} \$";
+  }
 }
 
 void main() {
