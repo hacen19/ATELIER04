4c1334dd111385e1660fdfb197e0df938fb6698b
hacen19 <hassenhemich@gmail.com>
2025-10-29 04:10:58 -0400

    Ex3 - Commit 4: Constructeur simplifié avec paramètre optionnel

--- exercice03.dart
+++ exercice03.dart
@@ -1,10 +1,10 @@
 // exercice03.dart
 
 class Compte {
-  double _solde = 0.0;
+  double _solde;
   final String numeroCompte;
   
-  Compte(this.numeroCompte);
+  Compte(this.numeroCompte, [this._solde = 0.0]);
 }
 
 void main() {
