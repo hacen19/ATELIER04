c1d953a3ddac4f8a5a68a596903a845c93b416b6
hacen19 <hassenhemich@gmail.com>
2025-10-29 11:52:17 -0400

    Ex8 - Commit 2: Ajout de _pages, getter et compteur statique totalLivres

--- exercice08.dart
+++ exercice08.dart
@@ -3,20 +3,32 @@
 class Livre {
   String titre;
   String auteur;
+  int _pages = 200;
+  static int totalLivres = 0;
 
-  Livre(this.titre, this.auteur);
+  Livre(this.titre, this.auteur) {
+    totalLivres++;
+  }
+
+  int get pages => _pages;
 
   void afficherInfos() {
-    print("Titre: $titre, Auteur: $auteur");
+    print("Titre: $titre, Auteur: $auteur, Pages: $_pages");
+  }
+
+  static void afficherTotal() {
+    print("Nombre total de livres créés: $totalLivres");
   }
 }
 
 void main() {
-  print("Exercice 8 - Classe Livre\n");
+  print("Exercice 8 - Classe Livre avec compteur\n");
 
   var livre1 = Livre("1984", "George Orwell");
   var livre2 = Livre("L'Étranger", "Albert Camus");
 
   livre1.afficherInfos();
   livre2.afficherInfos();
+
+  Livre.afficherTotal();
 }
